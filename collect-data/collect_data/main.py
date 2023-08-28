4b909efc76f75cc6ade334932807c44d25b31da6
oscar-cortez-home <oscarkortez@gmail.com>
2023-08-28 11:09:57 -0400

    added start and end date as a class to get dinamically the values

--- collect-data/collect_data/main.py
+++ collect-data/collect_data/main.py
@@ -1,5 +1,6 @@
 from application.binance_loader import BinanceLoader
 from config.data_yaml_generator import DataYamlGenerator
+from tools.binance_date_generator import BinanceDateGenerator 
 from tools.constants import Constants as C
 import sys
 
@@ -10,13 +11,15 @@ if len(sys.argv) > 1:
 dyg = DataYamlGenerator(yaml_file= C.PATH_CONFIG_SETTINGS_YAML)
 params = dyg.get_values(section)
 
+bdg = BinanceDateGenerator(type_data= section)
+
 bl = BinanceLoader(
     destination_source= params['destination_source'],
     table_name= params['table_name'],
     symbol= params['symbol'],
     interval= params['interval'],
-    start= params['start'],
-    end= params['end']
+    start= bdg.get_start(),
+    end= bdg.get_end()
     )
 
 bl.generate()
---
+++ collect-data/collect_data/tools/binance_date_generator.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta
+
+class BinanceDateGenerator:
+    
+    def __init__(self, type_data):
+
+        self.type_data = type_data
+    
+    def get_midnight_today(self):
+
+        current_day = datetime.utcnow()
+        start_time = datetime(current_day.year, current_day.month, current_day.day, 0, 0, 0)
+        start_time_milliseconds = int(start_time.timestamp() * 1000)
+        return str(start_time_milliseconds)
+
+    def get_start(self):
+        
+        if self.type_data == 'binance_historical_data':
+            return '1 Jan, 1990'
+        else:
+            return self.get_midnight_today()
+    
+    def get_end(self):
+        if self.type_data == 'binance_historical_data':
+            return '1 day ago UTC'
+        else:
+            return None
