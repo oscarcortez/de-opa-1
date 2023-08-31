d0b5366431b0867b14de5fd90c824e435aefbbfb
oscar-cortez-home <oscarkortez@gmail.com>
2023-08-31 00:18:17 -0400

    fixed bash to solve cron issue

--- collect-data/historical.sh
+++ collect-data/historical.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+CRYPTOBOT_ENV=production
 poetry run python3 /home/oscar/git-repos/de-opa-1/collect-data/collect_data/main.py historical
--- collect-data/streaming.sh
+++ collect-data/streaming.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+CRYPTOBOT_ENV=production
 cd /home/oscar/git-repos/de-opa-1/collect-data/ && /home/oscar/.local/bin/poetry run python3 /home/oscar/git-repos/de-opa-1/collect-data/collect_data/main.py streaming
