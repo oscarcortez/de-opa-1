3998ad00cadfbb629302fb5aecd3a6e5ee0c2d89
oscar-cortez-home <oscarkortez@gmail.com>
2023-08-31 01:18:19 -0400

    fixed env test bash file

--- collect-data/env.sh
+++ collect-data/env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export CRYPTOBOT_ENV=production
+export CRYPTOBOT_ENV="production"
 cd /home/oscar/git-repos/de-opa-1/collect-data/ && /home/oscar/.local/bin/poetry run python3 /home/oscar/git-repos/de-opa-1/collect-data/collect_data/main_env.py
 
