891fae3e58c321d4ed5d273779e43f37ca33e209
oscar-cortez-home <oscarkortez@gmail.com>
2023-08-31 00:49:50 -0400

    updated env sh

--- collect-data/env.sh
+++ collect-data/env.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 CRYPTOBOT_ENV=production
-/home/oscar/.local/bin/poetry run python3 /home/oscar/git-repos/de-opa-1/collect-data/collect_data/main_env.py
+cd /home/oscar/git-repos/de-opa-1/collect-data/ && /home/oscar/.local/bin/poetry run python3 /home/oscar/git-repos/de-opa-1/collect-data/collect_data/main_env.py
+
