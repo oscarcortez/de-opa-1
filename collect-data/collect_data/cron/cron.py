9c769695d1a18358f6960e6c2bd0940818c604da
Sven Wiesse <Sven@svens-mbp.speedport.ip>
2023-09-26 08:08:05 +0200

    init add

--- collect-data/collect_data/cron/cron.py
+++ collect-data/collect_data/cron/cron.py
@@ -1,15 +1,10 @@
-import schedule
-import urllib.request, json
-import time
-import pandas as pd
-from binance import Client
 #from tools.yaml_reader import YAMLReader
-#from tools.constants import Section
-from cron.creds import binance
+from collect_data.tools.constants import Section
 
 #client = Client(api_key, api_secret)
-api_key = binance(api_key)
-print(api_key)
+section = Section.BINANCE
+#api_key = 'test'
+print(section)
 
 #secrets_settings= YAMLReader
 #secrets_settings = secrets_settings
