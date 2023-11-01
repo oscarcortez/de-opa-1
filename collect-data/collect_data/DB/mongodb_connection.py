f42ef4ffa953fb8e7a509568a2635b18091ed638
sven008 <swiesse@gmx.de>
2023-11-01 07:24:11 +0100

    solved connection with mongodb

--- collect-data/collect_data/DB/mongodb_connection.py
+++ collect-data/collect_data/DB/mongodb_connection.py
@@ -5,8 +5,12 @@ from tools.constants import DB, RelativePath
 dyg = YAMLReader(yaml_file=RelativePath.ENV_SETTINGS)
 params = dyg.get_values(section=DB.MONGODB)
 
-mongo_client = MongoClient(
-                host=params['host'],
-                port=params['port'],
-                authSource="admin")
+MONGO_HOST = params['host'] 
+MONGO_PORT = params['port']
+MONGO_DB = params['drivername']
+MONGO_USER = params['username']
+MONGO_PASS = params['password']
+
+uri = "mongodb://{}:{}@{}:{}/{}?authSource=admin".format(MONGO_USER, MONGO_PASS, MONGO_HOST, MONGO_PORT, MONGO_DB)
+mongo_client = MongoClient(uri)
 mongo_db = mongo_client[params["database"]]
