26cf0518990ffe6d6a1a6c2e2fc732b47ff9d8fc
sven008 <swiesse@gmx.de>
2023-12-14 10:01:24 +0100

    Dash implementation to docker-compose

--- collect-data/collect_data/dash/main.py
+++ collect-data/collect_data/dash/main.py
@@ -8,11 +8,18 @@ from dash import dash_table
 from plotly.subplots import make_subplots
 import requests
 
-response = requests.get('http://localhost:8000/streaming/', headers={'accept': 'application/json'})
-data = response.json()
-df = pd.DataFrame(data['data'])
-df['open_time'] = pd.to_datetime(df['open_time'])
-fig = px.line(df, x='open_time', y='close_price', title='Close Price Over Time')
+response1 = requests.get('http://fastapi:8000/streaming/', headers={'accept': 'application/json'})
+response2 = requests.get('http://fastapi:8000/historical/', headers={'accept': 'application/json'})
+data1 = response1.json()
+data2 = response2.json()
+df_streaming = pd.DataFrame(data1['data'])
+df_historical = pd.DataFrame(data2['data'])
+df_streaming['open_time'] = pd.to_datetime(df_streaming['open_time'])
+df_historical['open_time'] = pd.to_datetime(df_historical['open_time'])
+fig1 = px.line(df_streaming, x='open_time', y='close_price', title='Streaming Data Bitcoin')
+fig2 = px.line(df_historical, x='open_time', y='close_price', title='Historical Data Bitcoin')
+
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets,suppress_callback_exceptions=True)
@@ -25,17 +32,15 @@ app.layout = html.Div([
     html.Hr(style={'borderWidth': "0.5vh", "width": "100%", "borderColor": "#F3DE8A","opacity": "unset"})
 ])
 
-
-
-
 @app.callback(dash.dependencies.Output('page-content', 'children'),
               [dash.dependencies.Input('url', 'pathname')])
                       
 def display_page(pathname):
     return html.Div([
-        html.H2('Streaming Data for BTC-USD'),
+        html.H2('Charts'),
         #html.Pre(children=str(df))
-        dcc.Graph(figure=fig)
+        dcc.Graph(figure=fig1),
+        dcc.Graph(figure=fig2)
     ])
 
 if __name__ == '__main__':
