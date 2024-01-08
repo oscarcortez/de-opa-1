f57e56e4d6f524f3e061cda8a6e6d3cfa301358c
oscar-cortez-home <oscarkortez@gmail.com>
2024-01-08 10:18:14 -0400

    fix about postgres url in docker and no docker envs

--- collect-data/collect_data/main_env.py
+++ collect-data/collect_data/main_env.py
@@ -1,8 +1,8 @@
 from tools.os_environment import os_environment
-import os
+# import os
 
 import datetime
 
 print("environment:", os_environment(), end=", ")
-print("login:", os.getlogin(), end=", ")
+# print("login:", os.getlogin(), end=", ")
 print("datetime:", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
--- collect-data/collect_data/routers/historical_data.py
+++ collect-data/collect_data/routers/historical_data.py
@@ -5,6 +5,7 @@ from application.binance_data_application import BinanceDataApplication
 from container.api_binance_data_container import ApiBinanceDataContainer
 from tools.yaml_reader import YAMLReader
 from tools.constants import Table, RelativePath
+from typing import Optional
 
 router = APIRouter(
     prefix="/historical",
@@ -14,15 +15,22 @@ router = APIRouter(
 
 
 @router.get("/")
-def get_streaming_data():
+def get_historical_data(start: Optional[int] = 1, end: Optional[int] = 50):
     container = ApiBinanceDataContainer(
         binance_api_settings=YAMLReader(RelativePath.BINANCE_API_SETTINGS),
         binance_data_application=BinanceDataApplication(),
     )
 
-    binanceDataApp = \
-        container.build_binance_data_application(Table.HISTORICAL_DATA)
-    df = binanceDataApp.find_all()
-    json_data = df.to_dict(orient="records")
-    return JSONResponse(content={"rows": len(json_data), "data": json_data},
-                        status_code=200)
+    binanceDataApp = container.build_binance_data_application(
+        Table.HISTORICAL_DATA
+    )
+    df_total = binanceDataApp.find_all()
+    df_segment = df_total[start-1:end]
+    json_data = df_segment.to_dict(orient="records")
+    return JSONResponse(
+        content={
+            "rows": f"{len(json_data)} of {len(df_total)}",
+            "data": json_data
+        },
+        status_code=200
+    )
