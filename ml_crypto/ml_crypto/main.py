4357bd470566a647292ef43dfba75165ebc86885
oscar-cortez-home <oscarkortez@gmail.com>
2024-02-14 09:39:19 -0400

    created ml_crypto project to test tensorflow

---
+++ ml_crypto/ml_crypto/main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+#from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import LSTM, Dense
+import matplotlib.pyplot as plt
+
+print('hello world')
