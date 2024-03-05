1ce96b00868df601a5c25c316ea21975c91d2517
sven008 <swiesse@gmx.de>
2024-03-05 07:51:31 +0100

    changes to dash

--- collect-data/collect_data/dash/main.py
+++ collect-data/collect_data/dash/main.py
@@ -2,9 +2,7 @@ import dash
 import pandas as pd
 from dash import html, dcc
 import plotly.express as px
-from dash.dependencies import Output, Input, State
-from dash import dash_table
-from plotly.subplots import make_subplots
+from dash.dependencies import Output, Input
 import requests
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
